20d9560eeb1723cc1178005e42e06b1e1f3cd8bf
leieo <leila.el.omari@gmail.com>
2018-04-04 10:49:56 +0200

    répartition correcte model vs. controller

--- models/project.php
+++ models/project.php
@@ -1,14 +1,24 @@
 <?php
 
-function create() {
+function createProject($user_id, $title) {
 
 	require __DIR__.'/../database.php';
 
-	echo 'first step ok';
+	$create = $database->prepare('INSERT INTO project(user_id, title) VALUES (:user_id, :title)');
 
-	}
+	$create->execute(array(
+			'user_id' => $user_id,
+			'title' => $title
+		));
 
+	echo 'Le projet \'' . $title . '\' vient d\'être créé !';
+
+	$create->closeCursor();
+}
+/*
 class Project {
+	public $user_id = 2;
+	public $title = 'in progress';
 
 	public function create() {
 
@@ -16,18 +26,22 @@ class Project {
 
 		echo 'first step youpi';
 
-		$user_id = 1;
-		$title = 'v1 working';
-
 		$create = $database->prepare('INSERT INTO project(user_id, title) VALUES (:user_id, :title)');
+
+		echo ' - second step ok';
+
+		echo ' - ' . $user_id . ' ' . $title ;
 
 		$create->execute(array(
 			'user_id' => $user_id,
 			'title' => $title
 		));
 
+		echo ' - third step all right';
+
 		echo ' - ' . $user_id . ' ' . $title ;
 
 		$create->closeCursor();
 	}
 }
+*/
--- pages/newproject.php
+++ pages/newproject.php
@@ -4,6 +4,15 @@ session_start();
 require __DIR__.'/../models/user.php';
 require __DIR__.'/../models/project.php';
 
-Project::create();
+function getInfos() {
+	$user_id = $_SESSION['id'];
+	$title = htmlspecialchars($_POST['title']);
+	return array ($user_id, $title);
+}
+
+if (isset($_POST['newproject'])) {
+	list ($user_id, $title) = getInfos();
+	createProject($user_id, $title);
+}
 
 require __DIR__.'/../views/newproject-view.php';
