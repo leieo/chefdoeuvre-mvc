3429e430d39bb91d2c0c0768e375e2412194a8f1
leieo <leila.el.omari@gmail.com>
2018-04-24 16:57:43 +0200

    suppression index-view

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
-<?php
-
-require __DIR__.'/views/index-view.php';
-
-
-// ommission volontaire de la balise fermante :
-// bonne pratique pour les fichiers ne contenant que du php
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>mindmap</title>
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Cutive+Mono&amp;subset=latin-ext" rel="stylesheet">
+</head>
+<body>
+	<header class="--center"><!-- alignement css -->
+	<br/>
+			<a href="./pages/login.php">Log in</a> | 
+			<a href="./pages/register.php">Register</a>
+	</header>
+	<main class="home">
+		<h1>Mindmap</h1>
+	</main>
+</body>
+</html>
